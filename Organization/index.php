030969064e2b935a2d66030fe79ec47981dc64f5
dubletab <gnevyshevdv@gmail.com>
2021-12-05 19:11:39 +0300

    add personal in element of structure

--- Organization/index.php
+++ Organization/index.php
@@ -1,14 +1,12 @@
 <?php
-
-include('/ConnectDB.php');
+include('/home/bitrix/php_scripts_bitrix24/ConnectDB.php');
 
 function insertData($link, $data){
-    $sguid = $data['guid'];
-    $sname = $data['name'];
+    $sguid = $data['Ссылка'];
+    $sname = $data['Наименование'];
     $snameUpper = strtoupper(($sname));
-    $scod = $data['code'];
-    $sparentCod = $data['parentCode'];
-    $sdescr = $data['descr'];
+    $scod = $data['Код'];
+    $sparentCod = $data['Родитель'];
 
 
     $queryMargin = "SELECT max(left_margin) as lmargin, max(right_margin) as rmargin from b_iblock_section where b_iblock_section.iblock_id = 11";
@@ -16,28 +14,37 @@ function insertData($link, $data){
     $rowMargin = mysqli_fetch_array($resultMargin);
     $lmargin = $rowMargin['lmargin'] + 1;
     $rmargin = $rowMargin['rmargin'] + 1;
-
+    $last_id;
+    $lvl;
 
     if($sparentCod == null){
         $query = "INSERT into b_iblock_section 
         (TIMESTAMP_X, MODIFIED_BY, DATE_CREATE, CREATED_BY, IBLOCK_ID, IBLOCK_SECTION_ID, ACTIVE, GLOBAL_ACTIVE, SORT, NAME, LEFT_MARGIN, RIGHT_MARGIN, DEPTH_LEVEL, DESCRIPTION, DESCRIPTION_TYPE, SEARCHABLE_CONTENT, CODE, XML_ID, TMP_ID) 
-        values (now(), 2317, now(), 2317, 11, 21, 'Y', 'Y', 500, '$sname', '$lmargin', '$rmargin', 2, '$sdescr', 'text', '$snameUpper', '$scod', '$sguid', '$sparentCod')";
+        values (now(), 2317, now(), 2317, 11, 21, 'Y', 'Y', 500, '$sname', '$lmargin', '$rmargin', 2, '$sname', 'text', '$snameUpper', '$scod', '$sguid', '$sparentCod')";
         $result = mysqli_query($link, $query);
         $last_id = mysqli_insert_id($link);
+
+
     }else{
         $query = "INSERT into b_iblock_section 
         (TIMESTAMP_X, MODIFIED_BY, DATE_CREATE, CREATED_BY, IBLOCK_ID, IBLOCK_SECTION_ID, ACTIVE, GLOBAL_ACTIVE, SORT, NAME, LEFT_MARGIN, RIGHT_MARGIN, DEPTH_LEVEL, DESCRIPTION, DESCRIPTION_TYPE, SEARCHABLE_CONTENT, CODE, XML_ID, TMP_ID) 
-        values (now(), 2317, now(), 2317, 11, null, 'Y', 'Y', 500, '$sname', '$lmargin', '$rmargin', null, '$sdescr', 'text', '$snameUpper', '$scod', '$sguid', '$sparentCod')";
+        values (now(), 2317, now(), 2317, 11, null, 'Y', 'Y', 500, '$sname', '$lmargin', '$rmargin', null, '$sname', 'text', '$snameUpper', '$scod', '$sguid', '$sparentCod')";
         $result = mysqli_query($link, $query);
         $last_id = mysqli_insert_id($link);
 
         /* если есть родитель ставим уровень вложенности*/
-        $queryParent = "SELECT ID, DEPTH_LEVEL from b_iblock_section where code = '$sparentCod'";
+        $queryParent = "SELECT ID, DEPTH_LEVEL from b_iblock_section where xml_id = '$sparentCod'";
         $resultParent = mysqli_query($link, $queryParent);
         $rowParent = mysqli_fetch_array($resultParent);
         $idParent = $rowParent['ID'];
         $lvlParent = $rowParent['DEPTH_LEVEL'];
-        $lvl = $lvlParent + 1;
+        if(is_numeric($lvlParent)) { 
+           $lvl = $lvlParent + 1;
+        } else { 
+           $lvl = 2;  
+           $idParent = 21;
+        }
+        
 
         $query = "UPDATE b_iblock_section
                     set 
@@ -59,25 +66,23 @@ function insertData($link, $data){
                             set
                             DEPTH_LEVEL = $lvl,
                             IBLOCK_SECTION_ID = $last_id
-                        where b_iblock_section.iblock_id = 11 and b_iblock_section.tmp_id = '$scod'";
+                        where b_iblock_section.iblock_id = 11 and b_iblock_section.tmp_id = '$sguid'";
     $resultUpdateChild = mysqli_query($link, $queryUpdateChild);
-
 }
 
 function updateData($link, $data){
-    $sguid = $data['guid'];
-    $sname = $data['name'];
+    $sguid = $data['Ссылка'];
+    $sname = $data['Наименование'];
     $snameUpper = strtoupper(($sname));
-    $scod = $data['code'];
-    $sparentCod = $data['parentCode'];
-    $sdescr = $data['descr'];
+    $scod = $data['Код'];
+    $sparentCod = $data['Родитель'];
 
 
     if($sparentCod == null){
         $query = "UPDATE b_iblock_section
         set
             b_iblock_section.name = '$sname',
-            b_iblock_section.DESCRIPTION = '$sdescr',
+            b_iblock_section.DESCRIPTION = '$sname',
             b_iblock_section.SEARCHABLE_CONTENT = '$snameUpper',
             b_iblock_section.CODE = '$scod',
             b_iblock_section.TMP_ID = null,
@@ -86,21 +91,26 @@ function updateData($link, $data){
         $result = mysqli_query($link, $query);
     }else{
         /* если есть родитель ставим уровень вложенности*/
-        $queryParent = "SELECT ID, DEPTH_LEVEL as dl from b_iblock_section where code = '$sparentCod'";
+        $queryParent = "SELECT ID, DEPTH_LEVEL as dl from b_iblock_section where xml_id = '$sparentCod'";
         $resultParent = mysqli_query($link, $queryParent);
         $rowParent = mysqli_fetch_array($resultParent);
         $idParent = $rowParent['ID'];
-        echo $idParent;
+
         $lvlParent = $rowParent['dl'];
-        echo '_' . $lvlParent . '_';
-        $lvl = $lvlParent + 1;
-        echo ' ' . $lvl;
+
+        if(is_numeric($lvlParent)) { 
+            $lvl = $lvlParent + 1;
+        } else { 
+            $lvl = 2;  
+            $idParent = 21;
+        }
+
 
 
         $query = "UPDATE b_iblock_section
         set
             b_iblock_section.name = '$sname',
-            b_iblock_section.DESCRIPTION = '$sdescr',
+            b_iblock_section.DESCRIPTION = '$sname',
             b_iblock_section.SEARCHABLE_CONTENT = '$snameUpper',
             b_iblock_section.CODE = '$scod',
             b_iblock_section.TMP_ID = '$sparentCod',
@@ -108,6 +118,8 @@ function updateData($link, $data){
             IBLOCK_SECTION_ID = $idParent
         where b_iblock_section.iblock_id = 11 and b_iblock_section.xml_id = '$sguid'";
         $result = mysqli_query($link, $query);
+
+
 
     }
 
@@ -124,34 +136,29 @@ function updateData($link, $data){
                             IBLOCK_SECTION_ID = $idForChild
                         where b_iblock_section.iblock_id = 11 and b_iblock_section.tmp_id = '$scod'";
     $resultUpdateChild = mysqli_query($link, $queryUpdateChild);
-
-
-    
-
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $data = json_decode(file_get_contents('php://input'), true);
-    echo $data['guid'];
-
     $link = connectToDB();
 
-    $sid = $data['guid'];
-    /* получаем все неотправленные сообщения из таблице исходящей очереди */
+    $sguid = $data['Ссылка'];
+    
+    /* получаем все неотправленные сообщения из таблици исходящей очереди */
     $query = "SELECT count(*) as ncount FROM b_iblock_section
-                where b_iblock_section.xml_id = '$sid' and b_iblock_section.iblock_id = 11";
+                where b_iblock_section.xml_id = '$sguid' and b_iblock_section.iblock_id = 11";
 
     $result = mysqli_query($link, $query);
 
     $row = mysqli_fetch_array($result);
 
     if ($row['ncount'] == 0) {
-        echo ' insert!';
+        echo 'insert:' . $sguid;
         insertData($link, $data);
     } else {
         updateData($link, $data);
-        echo ' upd!';
+        echo ' upd:' . $sguid;
     }
 
     mysqli_close($link);
@@ -160,4 +167,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 } else {
     header('HTTP/1.1 403 Deny method');
 }
+require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+CIBlockSection::ReSort(11);
 ?>
--- Personal/index.php
+++ Personal/index.php
@@ -1,60 +1,83 @@
 <?php
 
-include('/ConnectDB.php');
+include('/home/bitrix/php_scripts_bitrix24/ConnectDB.php');
 
 
-function insertData($link, $data, $idPropertyGUID){
-    $sid = $data['guid'];
-    $sname = $data['fullname'];
+function insertData($link, $data){
+    $idInfoBlock = 11;
+    $sid = $data['Ссылка'];
+    $sname = $data['Наименование'];
+    $sguidDepth = $data['Подразделение'];
     $snameUpper = strtoupper(($sname));
-    $idInfoBlock = 26;
 
+
+    // Поиск подразделения
+    $query = "SELECT id from b_iblock_section where xml_id = '$sguidDepth'";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+    $idIblockSection = $row['id'];
+    // Инсерт основных полей
     $query = "insert into b_iblock_element
-            (active, sort, name, searchable_content, code, iblock_id, date_create)
-            values ('Y', 500, '$sname', '$snameUpper', '$sid', $idInfoBlock, now())";
+            (timestamp_x, iblock_section_id, active, sort, name, searchable_content, code, iblock_id, date_create, in_sections)
+            values (now(), '$idIblockSection', 'Y', 500, '$sname', '$snameUpper', '$sid', $idInfoBlock, now(), 'Y')";
 
     $result = mysqli_query($link, $query);
     $last_id = mysqli_insert_id($link);
 
-    foreach ($data as $key => $value) {
 
-        $idProperty;
-        if($key == "firstName") $idProperty = 85;
-        elseif($key == "secondName") $idProperty = 86;
-        elseif($key == "middleName") $idProperty = 87;
-        elseif($key == "depthCode") $idProperty = 88;
-        elseif($key == "guid") $idProperty = 89;
-        elseif($key == "number") $idProperty = 90;
-        elseif($key == "position") $idProperty = 91;
-        elseif($key == "dateBirth") $idProperty = 92;
-        elseif($key == "sex") $idProperty = 93;
-        elseif($key == "fullname") $idProperty = 94;
-        elseif($key == "phone") $idProperty = 95;
-        else null;
 
-        $query = "insert into b_iblock_element_property
-        (iblock_property_id, iblock_element_id, value)
-        values ($idProperty, $last_id, '$value')";
-
-        $result = mysqli_query($link, $query);
         
-    }
+    $property101 = $data["Фамилия"];
+    $property102 = $data["Имя"];
+    $property103 = $data["Отчество"];
+    $property105 = $data["Подразделение"];
+    $property106 = $data["ПодразделениеКод"];
+    $propertyGUID = $data["Ссылка"];
+    $property100 = $data["Код"];
+    $property96 = $data["Должность"];
+    $property104 = $data["ДатаРождения"];
+    $property97 = $data["Телефон"];
+    $property98 = $data["Расположение"];
+  
+
+    $query = "INSERT into b_iblock_element_prop_s11
+    (iblock_element_id, property_96, property_97, property_98, property_100, property_101, property_102, property_103, property_104, property_105, property_106)
+    values ($last_id, '$property96', '$property97', '$property98', '$property100', '$property101', '$property102', '$property103', '$property104', '$property105', '$property106')";
+    $result = mysqli_query($link, $query);
+        
 
 
+    $query = "INSERT INTO b_iblock_section_element
+    (iblock_section_id, iblock_element_id)
+    values ($idIblockSection, $last_id)";
+    $result = mysqli_query($link, $query);
+
+    addInfoFromUsers($link, $data, $last_id);
 }
 
 
-function updateData($link, $data, $idPropertyGUID){
-    $sid = $data['guid'];
-    $sname = $data['fullname'];
+function updateData($link, $data){
+    $idInfoBlock = 11;
+    $sid = $data['Ссылка'];
+    $sname = $data['Наименование'];
+    $sguidDepth = $data['Подразделение'];
     $snameUpper = strtoupper(($sname));
-    $idInfoBlock = 26;
 
-    $query = "UPDATE b_iblock_element 
-                    SET 
+
+    // Поиск подразделения
+    $query = "SELECT id from b_iblock_section where xml_id = '$sguidDepth'";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+    $idIblockSection = $row['id'];
+    // Апдейт основных полей
+    $query = "UPDATE b_iblock_element
+                    set
+                        
+                        iblock_section_id = '$idIblockSection',
                         name = '$sname',
-                        searchable_content = '$snameUpper'
-                    WHERE
+                        searchable_content = '$snameUpper',
+                        code = '$sid'
+                    where
                         iblock_id = $idInfoBlock
                         AND
                         code = '$sid'";
@@ -62,81 +85,99 @@ function updateData($link, $data, $idPropertyGUID){
     $result = mysqli_query($link, $query);
 
 
-    /* Обновляем значения полей*/
-
-    $query = "SELECT 
-            b_iblock_element.id AS idinfoblock
-        FROM
-            b_iblock_element
-        WHERE
-            b_iblock_element.code = '$sid'";
-
-        $result = mysqli_query($link, $query);
-
-        $row = mysqli_fetch_array($result);
-        $idInfoBlockElement = $row['idinfoblock'];
+    $query = "SELECT id from b_iblock_element where code = '$sid'";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+    $last_id = $row['id'];
 
 
-    foreach ($data as $key => $value) {
+    $property101 = $data["Фамилия"];
+    $property102 = $data["Имя"];
+    $property103 = $data["Отчество"];
+    $property105 = $data["Подразделение"];
+    $property106 = $data["ПодразделениеКод"];
+    $propertyGUID = $data["Ссылка"];
+    $property100 = $data["Код"];
+    $property96 = $data["Должность"];
+    $property104 = $data["ДатаРождения"];
+    $property97 = $data["Телефон"];
+    $property98 = $data["Расположение"];
+    $query = "UPDATE b_iblock_element_prop_s11
+                set
+                    property_96 = '$property96',
+                    property_97 = '$property97',
+                    property_98 = '$property98',
+                    property_100 = '$property100',
+                    property_101 = '$property101',
+                    property_102 = '$property102',
+                    property_103 = '$property103',
+                    property_104 = '$property104',
+                    property_105 = '$property105',
+                    property_106 = '$property106'
+                where
+                    iblock_element_id = '$last_id'";
+    $result = mysqli_query($link, $query);
+        
 
 
-        $idProperty;
-        if($key == "firstName") $idProperty = 85;
-        elseif($key == "secondName") $idProperty = 86;
-        elseif($key == "middleName") $idProperty = 87;
-        elseif($key == "depthCode") $idProperty = 88;
-        elseif($key == "guid") $idProperty = 89;
-        elseif($key == "number") $idProperty = 90;
-        elseif($key == "position") $idProperty = 91;
-        elseif($key == "dateBirth") $idProperty = 92;
-        elseif($key == "sex") $idProperty = 93;
-        elseif($key == "fullname") $idProperty = 94;
-        elseif($key == "phone") $idProperty = 95;
-        else null;
-
-        $query = "UPDATE b_iblock_element_property 
-                    SET 
-                        value = '$value'
-                    WHERE
-                        iblock_element_id = $idInfoBlockElement
-                        AND
-                        iblock_property_id = '$idProperty'";
-
-        $result = mysqli_query($link, $query);
-
-    }
+    $query = "UPDATE b_iblock_section_element
+                set
+                    iblock_section_id = $idIblockSection
+                where
+                    iblock_element_id = $last_id";
+    $result = mysqli_query($link, $query);
+    
+    addInfoFromUsers($link, $data, $last_id);
 
 }
 
 
 
+function addInfoFromUsers($link, $data, $last_id){
+    $sname = $data['Наименование'];
+    $query = "SELECT count(*) as ncount FROM b_user where admin_notes = '$sname'";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+
+    if ($row['ncount'] == 1) {
+        $query = "SELECT personal_street, personal_phone FROM b_user where admin_notes = '$sname'";
+        $result = mysqli_query($link, $query);
+        $row = mysqli_fetch_array($result);
+        $persStreet = $row['personal_street'];
+        $persPhone = $row['personal_phone'];
+
+        $query = "UPDATE b_iblock_element_prop_s11
+            set
+                property_97 = '$persPhone',
+                property_98 = '$persStreet'
+            where
+                iblock_element_id = $last_id";
+        $result = mysqli_query($link, $query);
+
+    } else {
+        echo 'Сотрудник с таким ФИО не найден\несколько штук: ' . $sname;
+    }
+}
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $data = json_decode(file_get_contents('php://input'), true);
-    $idPropertyGUID = 89;
-    echo $data['guid'];
 
     $link = connectToDB();
 
-    $sid = $data['guid'];
+    $sid = $data['Ссылка'];
     /* получаем все неотправленные сообщения из таблице исходящей очереди */
-    $query = "SELECT count(*) as ncount FROM b_iblock_element
-                inner join b_iblock_property on b_iblock_element.iblock_id = b_iblock_property.iblock_id
-                inner join b_iblock_element_property on b_iblock_element_property.iblock_property_id = b_iblock_property.id and b_iblock_element_property.IBLOCK_ELEMENT_ID = b_iblock_element.id
-                where b_iblock_element_property.value = '$sid'
-                AND b_iblock_element_property.IBLOCK_PROPERTY_ID = $idPropertyGUID";
-
+    $query = "SELECT count(*) as ncount FROM b_iblock_element where code = '$sid'";
     $result = mysqli_query($link, $query);
-
     $row = mysqli_fetch_array($result);
 
     if ($row['ncount'] == 0) {
         echo ' insert!';
-        insertData($link, $data, $idPropertyGUID);
+         insertData($link, $data);
     } else {
-        updateData($link, $data, $idPropertyGUID);
-        echo ' upd!1';
+        updateData($link, $data);
+        echo ' upd!';
     }
 
     mysqli_close($link);
@@ -144,7 +185,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     header('HTTP/1.1 201 Created');
 } else {
     header('HTTP/1.1 403 Deny method');
-    echo 'Deny method';
 }
 
 ?>
